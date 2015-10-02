c33efe12ac3d274694e6c63055d7bef0dd1dfa9e
Hiroki Naganuma <hiroki11x@gmail.com>
2015-10-02 10:42:56 +0900

    to bind new proj

--- src/ModelDrawer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  3DModelDrawer.cpp
-//  MauInteractive
-//
-//  Created by 櫻木善将 on 2015/10/01.
-//
-//
-
-#include "ModelDrawer.h"
--- src/mode.cpp
+++
@@ -1,7 +0,0 @@
-//
-//  mode.cpp
-//  MauInteractive
-//
-//  Created by SakuragiYoshimasa on 2015/09/25.
-//
-//
