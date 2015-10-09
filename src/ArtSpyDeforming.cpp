1c10112d7b62246d14e7bf16ab32af6fca1cd5e5
櫻木善将 <ysrhsp@outlook.com>
2015-10-09 23:35:54 +0900

    artSpyのロゴ

---
+++ src/ArtSpyDeforming.cpp
@@ -0,0 +1,9 @@
+//
+//  ArtSpyDeforming.cpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/09.
+//
+//
+
+#include "ArtSpyDeforming.hpp"
---
+++ src/ArtSpyDeforming.h
@@ -0,0 +1,14 @@
+//
+//  ArtSpyDeforming.hpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/09.
+//
+//
+
+#ifndef ArtSpyDeforming_hpp
+#define ArtSpyDeforming_hpp
+
+#include <stdio.h>
+
+#endif /* ArtSpyDeforming_hpp */
