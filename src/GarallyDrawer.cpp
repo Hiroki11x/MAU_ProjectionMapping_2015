c3ad29a148e0304e43916ac4539e59026e8f5132
櫻木善将 <ysrhsp@outlook.com>
2015-10-09 22:17:30 +0900

    gar allyと顔の共存 ゲージが表示されない問題解決 trailersの消去 split画面実装

---
+++ src/GarallyDrawer.cpp
@@ -0,0 +1,9 @@
+//
+//  GarallyDrawer.cpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/09.
+//
+//
+
+#include "GarallyDrawer.hpp"
---
+++ src/GarallyDrawer.h
@@ -0,0 +1,14 @@
+//
+//  GarallyDrawer.hpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/09.
+//
+//
+
+#ifndef GarallyDrawer_hpp
+#define GarallyDrawer_hpp
+
+#include <stdio.h>
+
+#endif /* GarallyDrawer_hpp */
