07063b259881b0bbe1b4930816e1c11e9ecd7712
Hiroki Naganuma <hiroki11x@gmail.com>
2015-10-12 13:51:43 +0900

    create new element

--- src/SingleAgent.cpp
+++ src/SingleAgent.cpp
@@ -7,3 +7,81 @@
 //
 
 #include "SingleAgent.h"
+
+
+void SingleAgent::init(){
+    ofSetCircleResolution(32);
+    registertime = ofGetSystemTime();
+    icon.allocate(300, 300,OF_IMAGE_COLOR);
+    set_position(ofVec2f(ofGetWidth()/2,ofGetHeight()/2));
+}
+
+void SingleAgent::get_info_from_twitter(string name,string id, string text,int friends_count,int statuses_count,int followers_count, ofImage image){
+    //twitterからの取得情報を書く
+    username = name;
+    this->id = id;
+    this->text = text;
+    this->friends_count = friends_count;
+    this->statuses_count = statuses_count;
+    this->followers_count = followers_count;
+    icon = image;
+}
+
+void SingleAgent::set_position(ofVec2f _pos){
+    position = _pos;
+}
+
+void SingleAgent::set_size(float _size){
+    size = _size;
+}
+void SingleAgent::set_color(ofColor _color){
+    color = _color;
+}
+
+void SingleAgent::update(){
+}
+void SingleAgent::draw_line(float x, float y){
+    ofPushMatrix();
+    ofTranslate(x, y);
+    ofFill();
+    ofSetColor(0,255,0,ofRandom(0,170));
+    ofRect(0,-10,150,10);
+    ofSetColor(255);
+    FontManager::mfont.drawString(username, 0,0);
+    FontManager::mfont.drawString(id, 0,10);
+    FontManager::mfont.drawString("Follower: "+ofToString(followers_count), 0,20);
+    FontManager::mfont.drawString("Friends: "+ofToString(friends_count), 0,30);
+    FontManager::mfont.drawString("Statue: "+ofToString(statuses_count), 0,40);
+    ofPopMatrix();
+}
+
+void SingleAgent::draw_circle(){
+    int angle;
+    
+    ofFill();
+    ofSetColor(color);
+    ofCircle(position, size * multiple_of_size/2);
+    ofSetColor(255);
+    icon.draw(position.x-100,position.y-100, 200,200);
+    ofSetColor(color);
+    ofNoFill();
+    
+    ofBeginShape();//回転する内部の円
+    for(int i=0;i<240;i++){
+        angle = i + ofGetElapsedTimef()*50;
+        ofVec2f basis_vec =ofVec2f(sin(angle/180.0*PI), cos(angle/180.0*PI));
+        ofVec2f vec = position+ basis_vec*INNER_CIRCLE_MAGNIFICATION*size;
+        ofVertex(vec.x, vec.y);
+    }
+    ofEndShape();
+    
+    ofBeginShape();//回転する外部の円
+    for(int i=0;i<240;i++){
+        angle = 100 + i + ofGetElapsedTimef()*50;
+        ofVec2f basis_vec =ofVec2f(sin(-angle/180.0*PI), cos(-angle/180.0*PI));
+        ofVec2f vec = position + basis_vec*size;
+        ofVertex(vec.x, vec.y);
+    }
+    ofEndShape();
+    
+}
--- src/SingleAgent.h
+++ src/SingleAgent.h
@@ -9,6 +9,45 @@
 #ifndef __mauInteractive__SingleAgent__
 #define __mauInteractive__SingleAgent__
 
-#include <stdio.h>
+#include "ofMain.h"
+#include "FontManager.h"
+
+class SingleAgent{
+    
+private:
+    
+    float line_y1 = ofGetHeight()/2;
+    float line_y2 = 3*ofGetHeight()/2;
+    
+    float size;
+    float multiple_of_size;
+    float line_length;
+    
+    ofImage icon;
+    string id;
+    string username;
+    string text;//つぶやいた内容(これだけ["user"]内ではない)
+    int friends_count;//フォローしてるアカウント数
+    int statuses_count;//つぶやいてる数
+    int followers_count;
+    float registertime;
+    
+    ofColor color;
+    
+public:
+    ofVec2f position;
+    SingleAgent(){};
+    
+    void set_position(ofVec2f _pos);
+    void set_size(float _size);
+    void set_color(ofColor _color);
+    
+    void update();
+    void draw_circle();
+    void draw_line(float x, float y);
+    void init();
+    
+    void get_info_from_twitter(string name,string id, string text,int friends_count,int statuses_count,int followers_count, ofImage image);//twitterからの取得情報を書く
+};
 
 #endif /* defined(__mauInteractive__SingleAgent__) */
