e084175bbc9554eff612a12dc0f0c483ec14e9b8
櫻木善将 <ysrhsp@outlook.com>
2015-10-16 01:08:19 +0900

    gar allyのguy

---
+++ src/GarallySceneGuiDrawer.cpp
@@ -0,0 +1,9 @@
+//
+//  GarallySceneGuiDrawer.cpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/15.
+//
+//
+
+#include "GarallySceneGuiDrawer.hpp"
---
+++ src/GarallySceneGuiDrawer.h
@@ -0,0 +1,14 @@
+//
+//  GarallySceneGuiDrawer.hpp
+//  MauInteractive
+//
+//  Created by 櫻木善将 on 2015/10/15.
+//
+//
+
+#ifndef GarallySceneGuiDrawer_hpp
+#define GarallySceneGuiDrawer_hpp
+
+#include <stdio.h>
+
+#endif /* GarallySceneGuiDrawer_hpp */
