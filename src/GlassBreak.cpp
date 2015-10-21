134d20fdeb28b0cbcf3d6939811dc1eb840e79db
櫻木善将 <ysrhsp@outlook.com>
2015-10-21 14:50:13 +0900

    reset可能に

--- src/GlassBreak.cpp
+++ src/GlassBreak.cpp
@@ -13,18 +13,6 @@ void GlassBreak::init(){
     glassTriangleNum = glassModel.getMesh(0).indices.size()/3.0;
     glassMesh.useColors = true;
     reset();
-   /* for(int i = 0; i < glassModel.getMesh(0).indices.size(); i++){
-        glassMesh.addVertex(glassModel.getMesh(0).vertices.at(glassModel.getMesh(0).indices.at(i)) * 35);
-        glassMesh.addColor(ofColor::fromHsb(ofRandom(255), ofRandom(100,255), 255, ofRandom(100,255)));
-    }
-    
-    
-    for(int i = 0; i < glassTriangleNum; i++){
-        crashSpeeds.push_back(ofVec3f(glassMesh.vertices.at(i * 3).x * 0.01 + ofRandom(-3.0, 3.0),glassMesh.vertices.at(i * 3).y * 0.01 - 10, ofRandom(15)));
-        crashRotation.push_back(ofVec3f(ofRandom(-3.0, 3.0),ofRandom(-3.0, 3.0),ofRandom(-3.0, 3.0)));
-    }
-    drawCounter = 0;
-    mode = DrawGlass;*/
 }
 
 void GlassBreak::update(){
@@ -84,6 +72,7 @@ void GlassBreak::draw(){
 void GlassBreak::onMouseDown(int x, int y){}
 void GlassBreak::reset(){
     glassMesh.clear();
+    drawingMesh.clear();
     for(int i = 0; i < glassModel.getMesh(0).indices.size(); i++){
         glassMesh.addVertex(glassModel.getMesh(0).vertices.at(glassModel.getMesh(0).indices.at(i)) * 35);
         glassMesh.addColor(ofColor::fromHsb(ofRandom(255), ofRandom(100,255), 255, ofRandom(100,255)));
@@ -95,6 +84,7 @@ void GlassBreak::reset(){
     mode = DrawGlass;
     drawCounter = 0;
     spentFrames = 0;
+    addedVertex = 0;
 }
 
 void GlassBreak::keyPressed(int key){
